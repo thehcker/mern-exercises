e0d8b9bcc6c403a30ffd8d655ef1bfa84a743ad7
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2021-11-18 22:30:25 +0300

    Pass Params in Edit Exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./component/navbar.component";
+import ExcercisesList from "./component/exercises-list.component";
+import EditExercise from "./component/edit-exercise.component";
+import CreateExercise from "./component/create-exercise.component";
+import CreateUser from "./component/create-user.component";
+
 function App() {
   return (
     <Router>
-      <NavBar />
-      <br />
-      <Route path="/" exact component={ExcerciseList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <Switch>
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+        <Route path="/" exact component={ExcercisesList} />
+      </Switch>
     </Router>
   );
 }
